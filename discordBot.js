9ce0531d4b4505f16e3fd9fd2a067cc0706078ac
tomforster <tomforster@users.noreply.github.com>
2015-12-23 12:20:16 +0000

    Update discordBot.js

--- discordBot.js
+++ discordBot.js
@@ -6,6 +6,7 @@ var config = require(path.join(__dirname,'config/config.json'))[env];
 var mybot = new Discord.Client();
 
 mybot.on("message", function(message){
+    console.log(message.mentions.has('username','AppBot'));
     if(message.mentions.has('username','AppBot')){
         console.log(message);
         mybot.reply(message, "Hi, "+message.author.username);
