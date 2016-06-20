b0de288b3ff8392ab5f1ce723ccdefc2c83dcb6b
Tom Forster <tom.forster@tracsis.com>
2016-06-20 15:36:23 +0100

    changing code order

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ var logger = new (winston.Logger)({
         new (winston.transports.Console)({'timestamp':true})
     ]
 });
-var WebSocketServer = require('ws').Server;
 
 var sys = require('sys');
 var exec = require('child_process').exec;
 
 var app = express();
+var expressWs = require('express-ws')(app);
 
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -260,23 +260,3 @@ app.listen(port,function(){
     }, 30000);
 
 });
-
-var wss = new WebSocketServer({ server: app });
-
-wss.on('connection', function connection(ws) {
-
-    logger.info("Websocket Connected");
-
-});
-
-wss.on('error', function connection(error) {
-
-    logger.error(error);
-
-});
-
-wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-        client.send(data);
-    });
-};
