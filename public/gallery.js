ad26885fc6099ad0de1d62f8e955491d75ca4f8d
Tom Forster <tom.forster@tracsis.com>
2016-06-21 17:03:52 +0100

    remove some dead files

--- public/gallery.js
+++ public/gallery.js
@@ -1,11 +1,20 @@
 angular.module('pics',[])
 
 .controller('ctrl',["$http", '$window', function($http, $window){
+    
+    var self = this;
+    
     this.snapshot = function(){
         $http({
             method: 'POST',
             url: '/snapshot/1'});
+        self.snapshotInProgress = true;
+        setTimeout(function(){
+            self.snapshotInProgress = false;
+        }, 20000);
     };
+    
+    this.snapshotInProgress = false;
 
     var new_uri;
     if ($window.location.protocol === "https:") {
