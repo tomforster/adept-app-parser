a5db9822c6b575059374d0a7837e5826f8e3cc13
Tom <tomforster@gmail.com>
2016-06-25 12:43:27 +0100

    bump phantom version

--- phantomScripts.js
+++ phantomScripts.js
@@ -9,105 +9,116 @@ var logger = new (winston.Logger)({
     ]
 });
 
-postApp = function(mailObj){
+var postApp = function(mailObj){
     logger.info('Posting Adept App');
     var username = config.forumUsername;
     var password = config.forumPassword;
     return new Promise(function(fulfill,reject) {
-        phantom.create(function (ph) {
-            ph.createPage(function (page) {
-                page.open(config.forumUrl, function (status) {
-                    logger.info("opened page? status: ", status);
-
-                    //todo retry on bad status
-                    if (status.indexOf('success') == -1) {
-                        logger.info("exiting");
-                        ph.exit();
-                        reject();
-                        return;
-                    }
-
-                    logger.info("logging in as " + username + ":" + password + "...");
-                    page.evaluate(function (mailObj, username, password) {
-                        var usernameElement = document.querySelector('#username');
-                        if (!usernameElement) {
-                            return false;
-                        }
-                        usernameElement.value = username;
-                        document.querySelector('#password').value = password;
-                        document.querySelector('.button1[name=login]').click();
-                        return true;
-                    }, function (result) {
-                        //page.render('testfile.jpeg',{format: 'jpeg', quality: '100'});
-                        if (!result) {
-                            logger.info('No username detected');
-                            ph.exit();
-                            reject();
-                            return;
-                        }
-                        logger.info("waiting for load...");
+        var _ph, _page;
+        phantom.create().then(function(ph){
+            _ph = ph;
+            return _ph.createPage();
+        }).then(function(page) {
+            _page = page;
+            return _page.open(config.forumUrl);
+        }).then(function(status) {
+            logger.info("opened page? status: ", status);
+            if (status.indexOf('success') == -1) {
+                reject('Did not load');
+                logger.error("Failed to load page");
+                _ph.exit();
+            }
+            logger.info("logging in as " + username + "...");
+            _page.evaluate(function (mailObj, username, password) {
+                var usernameElement = document.querySelector('#username');
+                if (!usernameElement) {
+                    return false;
+                }
+                usernameElement.value = username;
+                document.querySelector('#password').value = password;
+                document.querySelector('.button1[name=login]').click();
+                return true;
+            }, function (result) {
+                //page.render('testfile.jpeg',{format: 'jpeg', quality: '100'});
+                if (!result) {
+                    logger.info('No username detected');
+                    ph.exit();
+                    reject();
+                    return;
+                }
+                logger.info("waiting for load...");
+                setTimeout(function () {
+                    logger.info("finished waiting, posting app:");
+                    _page.evaluate(function (mailObj) {
+                        document.querySelector('#subject').value = mailObj.title;
+                        document.querySelector('#message').value = mailObj.body;
+                        document.querySelector('.default-submit-action[name=post]').click();
+                    }, function () {
                         setTimeout(function () {
-                            logger.info("finished waiting, posting app:");
-                            page.evaluate(function (mailObj) {
-                                document.querySelector('#subject').value = mailObj.title;
-                                document.querySelector('#message').value = mailObj.body;
-                                document.querySelector('.default-submit-action[name=post]').click();
-                            }, function () {
-                                setTimeout(function () {
-                                    page.evaluate(function () {
-                                        return document.URL.split('&sid')[0]
-                                    }, function (url) {
-                                        if (url.indexOf('t=') > -1) {
-                                            logger.info('app posted!');
-                                            logger.info(url);
-                                            fulfill(url);
-                                        } else {
-                                            reject();
-                                            logger.info("Possibly did not upload");
-                                        }
-                                        logger.info('Exiting');
-                                        ph.exit();
-                                    });
-                                }, 10000);
-                            }, mailObj);
+                            page.evaluate(function () {
+                                return document.URL.split('&sid')[0]
+                            }, function (url) {
+                                if (url.indexOf('t=') > -1) {
+                                    logger.info('app posted!');
+                                    logger.info(url);
+                                    fulfill(url);
+                                } else {
+                                    reject();
+                                    logger.info("Possibly did not upload");
+                                }
+                                logger.info('Exiting');
+                                ph.exit();
+                            });
                         }, 10000);
-                    }, mailObj, username, password);
-                });
-            });
+                    }, mailObj);
+                }, 10000);
+            }, mailObj, username, password);
         });
     });
 };
 
-readAudit = function(){
+var readAudit = function(){
     logger.info("Starting audit read");
     return new Promise(function(resolve,reject){
         var auditUrl = "http://www.guildaudit.com/g/9146";
-        phantom.create(function (ph) {
-            ph.createPage(function (page) {
-                page.open(auditUrl, function (status) {
-                    logger.info("opened page? status: ", status);
-                    if (status.indexOf('success') == -1) {
-                        reject('Did not load');
-                        logger.error("Failed to load page");
-                        ph.exit();
-                    }
-                    page.evaluate(function () {
-                        logger.info("Reading data");
-                        var characterData = [];
-                        var names = $(".characterRow .nameLink").map(function(index,value){return $(value).text()});
-                        var ilvls = $(".characterRow .iLvl").map(function(index,value){return $(value).text()});
-                        var upgradesPC = $(".characterRow .upgradesNumber").map(function(index,value){return $(value).text()});
-                        var auditInfo = $(".characterRow .audit").map(function(index,value){return $(value).text()});
-                        for(var i = 0; i < names.length; i++){
-                            characterData.push({name:names[i],ilvl:ilvls[i],upgrades:upgradesPC[i],audit:auditInfo[i]});
-                        }
-                        return characterData;
-                    }, function (characterData) {
-                        logger.info("Got character data");
-                        resolve(characterData);
-                        ph.exit();
-                    });
+        var _ph, _page;
+        
+        phantom.create().then(function(ph){
+            _ph = ph;
+            return _ph.createPage();
+        }).then(function(page) {
+            _page = page;
+            return _page.open(auditUrl);
+        }).then(function(status) {
+            logger.info("opened page? status: ", status);
+            if (status.indexOf('success') == -1) {
+                reject('Did not load');
+                logger.error("Failed to load page");
+                _ph.exit();
+            }
+            logger.info("Reading data");
+            _page.evaluate(function () {
+                var characterData = [];
+                var names = $(".characterRow .nameLink").map(function (index, value) {
+                    return $(value).text()
                 });
+                var ilvls = $(".characterRow .iLvl").map(function (index, value) {
+                    return $(value).text()
+                });
+                var upgradesPC = $(".characterRow .upgradesNumber").map(function (index, value) {
+                    return $(value).text()
+                });
+                var auditInfo = $(".characterRow .audit").map(function (index, value) {
+                    return $(value).text()
+                });
+                for (var i = 0; i < names.length; i++) {
+                    characterData.push({name: names[i], ilvl: ilvls[i], upgrades: upgradesPC[i], audit: auditInfo[i]});
+                }
+                return characterData;
+            }).then(function (characterData) {
+                logger.info("Got character data");
+                resolve(characterData);
+                _ph.exit();
             });
         });
     });
