fb08f54937d45fe8327a62a6f7dcd495ca8788a6
Tom Forster <tom.forster@tracsis.com>
2016-08-17 15:56:36 +0100

    add duration to sql query

--- discordBot.js
+++ discordBot.js
@@ -73,6 +73,8 @@ bot.on("message", (message) => {
                         opMessage += duration > 0 ? ("the last " + humanizeDuration(duration) + "\n") : "all time:\n";
                         result.forEach(messageCount => opMessage += "\n" + messageCount.username + ": " + messageCount.count);
                         return bot.sendMessage(message.channel, opMessage);
+                    }else if(result.length = 0){
+                        return bot.sendMessage(message.channel, "No eligible messages found.");
                     }
                 }).catch(error => logger.error(error));
                 break;
