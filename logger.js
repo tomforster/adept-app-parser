7e222ca8191605d1d1efbceecac75f284e4c8fd0
Tom <tomforster@gmail.com>
2016-08-29 19:03:06 +0100

    add in timestamp logging

--- logger.js
+++ logger.js
@@ -8,20 +8,12 @@ winston.emitErrs = true;
 
 var logger = new winston.Logger({
     transports: [
-        // new winston.transports.File({
-        //     level: 'info',
-        //     filename: './logs/all-logs.log',
-        //     handleExceptions: true,
-        //     json: true,
-        //     maxsize: 5242880, //5MB
-        //     maxFiles: 5,
-        //     colorize: false
-        // }),
         new winston.transports.Console({
             level: 'debug',
             handleExceptions: true,
             json: false,
-            colorize: true
+            colorize: true,
+            timestamp: true
         })
     ],
     exitOnError: false
