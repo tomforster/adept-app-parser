2613ee0d724a6cb86c65561253c7d5b53423b323
Tom <tomforster@gmail.com>
2016-10-23 15:47:47 +0100

    some debugging for app posting

--- phantomScripts.js
+++ phantomScripts.js
@@ -17,6 +17,7 @@ var postApp = function(mailObj){
                 .innerText;
         })
         .then(function(title){
+            log.debug("loaded page", title);
             if (title.indexOf('Login') > -1) {
                 return nightmare
                     .insert('#username', username)
@@ -26,6 +27,7 @@ var postApp = function(mailObj){
                     .wait('#subject')
             }
         }).then(function(){
+            log.debug("loaded post page");
             return nightmare.insert('#subject', mailObj.title)
             .insert('#message', mailObj.body)
             .wait(2000)
@@ -36,56 +38,6 @@ var postApp = function(mailObj){
         })
 };
 
-// var readAudit = function(){
-    // log.info("Starting audit read");
-    // return new Promise(function(resolve,reject){
-    //     var auditUrl = "http://www.guildaudit.com/g/9146";
-    //     var _ph, _page;
-    //
-    //     phantom.create(['--load-images=no']).then(function(ph){
-    //         _ph = ph;
-    //         return _ph.createPage();
-    //     }).then(function(page) {
-    //         _page = page;
-    //         return _page.open(auditUrl);
-    //     }).then(function(status) {
-    //         log.info("opened page? status: ", status);
-    //         if (status.indexOf('success') == -1) {
-    //             reject('Did not load');
-    //             log.error("Failed to load page");
-    //             _ph.exit();
-    //         }
-    //         log.info("Reading data");
-    //         _page.evaluate(function () {
-    //             var auditData = {characterData:[], lastCheck:""};
-    //             var names = $(".characterRow .nameLink").map(function (index, value) {
-    //                 return $(value).text()
-    //             });
-    //             var ilvls = $(".characterRow .iLvl").map(function (index, value) {
-    //                 return $(value).text()
-    //             });
-    //             var upgradesPC = $(".characterRow .upgradesNumber").map(function (index, value) {
-    //                 return $(value).text()
-    //             });
-    //             var auditInfo = $(".characterRow .audit").map(function (index, value) {
-    //                 return $(value).text()
-    //             });
-    //             for (var i = 0; i < names.length; i++) {
-    //                 auditData.characterData.push({name: names[i], ilvl: ilvls[i], upgrades: upgradesPC[i], audit: auditInfo[i]});
-    //             }
-    //             var lastCheck = $(".groupLastChecked").text();
-    //             auditData.lastCheck = lastCheck.split(': ').pop();
-    //             return auditData;
-    //         }).then(function (auditData) {
-    //             log.info("Got audit data");
-    //             resolve(auditData);
-    //             _ph.exit();
-    //         });
-    //     });
-    // });
-// };
-
 module.exports = {
     postApp: postApp
-    //,readAudit: readAudit
 };
