12cf62b2f2c6125ace52c3c139973c9bb4adb085
Tom Forster <tom.forster@tracsis.com>
2017-02-17 13:21:15 +0000

    pmx config

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 'use strict';
 
 const pmx = require('pmx').init({
-    http: true, // HTTP routes logging (default: true)
-    errors: true, // Exceptions loggin (default: true)
-    custom_probes: true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
     network: true, // Network monitoring at the application level
-    ports: true  // Shows which ports your app is listening on (default: false)
 });
 
 const express = require('express');
