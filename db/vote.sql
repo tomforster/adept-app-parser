02e3ddd4652442c12b3db546f97104a385a9ff5c
Tom <tomforster@gmail.com>
2017-02-22 22:47:45 +0000

    upvote + downvote attempt 2

--- db/vote.sql
+++ db/vote.sql
@@ -3,8 +3,11 @@ CREATE TABLE public.vote
     id SERIAL PRIMARY KEY,
     image INTEGER NOT NULL,
     date INTEGER NOT NULL,
-    "user" INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     is_upvote BOOLEAN DEFAULT FALSE  NOT NULL,
     CONSTRAINT vote_command_id_fk FOREIGN KEY (image) REFERENCES command (id),
     CONSTRAINT vote_discord_user_id_fk FOREIGN KEY ("user") REFERENCES discord_user (id)
 );
+
+ALTER TABLE public.vote
+ ADD CONSTRAINT vote_image_user_is_upvote_uk UNIQUE (image, user_id, is_upvote);
