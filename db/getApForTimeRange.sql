a73d6cedcb3016e5a1602c808497a31b26aa79f5
Tom Forster <tom.forster@tracsis.com>
2017-02-22 12:59:49 +0000

    pmx config

---
+++ db/getApForTimeRange.sql
@@ -0,0 +1,6 @@
+select cs.name, max(cs.totalap::NUMERIC) - min(cs.totalap::NUMERIC) as total
+from (
+       select date, character_stats ->> 'name' as name, character_stats ->> 'totalAP' as totalap from audit where type='character_stats' and date < 1487674800
+     ) as cs
+group by name
+order by total desc;
