a03ab68b88dc792cf02717a5bde75f49d968a164
Tom <tomforster@gmail.com>
2017-04-01 23:44:36 +0100

    vote multiple times fixes #8

--- repositories/voteRepository.js
+++ repositories/voteRepository.js
@@ -7,19 +7,31 @@ const moment = require('moment');
 const log = require('bristol');
 
 exports.upvote = function(imageId, userId){
-    return db.none("insert into vote (image, user_id, date, is_upvote) values ($1, $2, $3, true)", [imageId, userId, moment().unix()]).then(() => true).catch(err => false);
+    return db.result(`insert into vote (image, user_id, date, is_upvote) 
+    select $1, $2, $3, true 
+    where not exists (
+    select * from vote where image = $1 and user_id = $2 and is_upvote = true and date between $3 - 60*60*24*7 and $3
+    )`, [imageId, userId, moment().unix()])
+        .then(result => result.rowCount > 0).catch(err => false);
 };
 
 exports.downvote = function(imageId, userId){
-    return db.none("insert into vote (image, user_id, date) values ($1, $2, $3)",[imageId, userId, moment().unix()]).then(() => true).catch(err => false);
+    return db.result(`insert into vote (image, user_id, date) 
+    select $1, $2, $3 
+    where not exists (
+    select * from vote where image = $1 and user_id = $2 and is_upvote = false and date between $3 - 60*60*24*7 and $3
+    )`, [imageId, userId, moment().unix()])
+        .then(result => result.rowCount > 0).catch(err => false);
 };
 
 exports.deleteUpvote = function(imageId, userId){
-    return db.result("delete from vote where image = $1 and user_id = $2 and is_upvote", [imageId, userId]).then(result => result.rowCount > 0).catch(err => false);
+    return db.result("delete from vote where image = $1 and user_id = $2 and is_upvote and date between $3 - 60*60*24*7 and $3", [imageId, userId, moment().unix()])
+        .then(result => result.rowCount > 0).catch(err => false);
 };
 
 exports.deleteDownvote = function(imageId, userId){
-    return db.result("delete from vote where image = $1 and user_id = $2 and not is_upvote", [imageId, userId]).then(result => result.rowCount > 0).catch(err => false);
+    return db.result("delete from vote where image = $1 and user_id = $2 and not is_upvote and date between $3 - 60*60*24*7 and $3", [imageId, userId, moment().unix()])
+        .then(result => result.rowCount > 0).catch(err => false);
 };
 
 exports.getVotes = function(imageId){
