83017426b91c3c1262b7a8a0a9499664c7265f5c
Tom <tomforster@gmail.com>
2017-06-03 19:41:56 +0100

    image stats command

--- discord/commands/image-stats.js
+++ discord/commands/image-stats.js
@@ -5,46 +5,54 @@
 const voteRepository = require("../../repositories/voteRepository");
 
 function run(message) {
-    let highestAvg = voteRepository.getUsersWithHighestAverageScore(3);
-    let lowestAvg = voteRepository.getUsersWithLowestAverageScore(3);
-    let highestTotal = voteRepository.getUsersWithHighestScore(3);
-    let lowestTotal = voteRepository.getUsersWithLowestScore(3);
+    const highestAvgPromise = voteRepository.getUsersWithHighestAverageScore(3);
+    const lowestAvgPromise = voteRepository.getUsersWithLowestAverageScore(3);
+    const highestTotalPromise = voteRepository.getUsersWithHighestScore(3);
+    const lowestTotalPromise = voteRepository.getUsersWithLowestScore(3);
 
-    let output = "";
-    if(highestAvg && highestAvg.length > 0){
-        output += "**Top 3 users by image score:**\n";
-        highestAvg.forEach((user, index) => {
-            output += index + ": " + user.username + " [" + user.total + "]\n"
-        });
-        output += "\n";
-    }
-    if(lowestAvg && lowestAvg.length > 0){
-        output += "**Bottom 3 users by image score:**\n";
-        lowestAvg.forEach((user, index) => {
-            output += index + ": " + user.username + " [" + user.total + "]\n"
-        });
-        output += "\n";
-    }
-    if(highestTotal && highestTotal.length > 0){
-        output += "**Top 3 users by average image score:**\n";
-        highestTotal.forEach((user, index) => {
-            output += index + ": " + user.username + " [" + (Math.round(user.total * 100) / 100) + "]\n"
-        });
-        output += "\n";
-    }
-    if(lowestTotal && lowestTotal.length > 0){
-        output += "**Bottom 3 users by average image score:**\n";
-        lowestTotal.forEach((user, index) => {
-            output += index + ": " + user.username + " [" + (Math.round(user.total * 100) / 100) + "]\n"
-        });
-        output += "\n";
-    }
+    return Promise.all([highestAvgPromise, lowestAvgPromise, highestTotalPromise, lowestTotalPromise]).then(result =>
+    {
+        const highestAvg = voteRepository.getUsersWithHighestAverageScore(3);
+        const lowestAvg = voteRepository.getUsersWithLowestAverageScore(3);
+        const highestTotal = voteRepository.getUsersWithHighestScore(3);
+        const lowestTotal = voteRepository.getUsersWithLowestScore(3);
 
-    return message.channel.sendMessage("```css\n" + output + "```");
+        let output = "";
+        if(highestAvg && highestAvg.length > 0){
+            output += "**Top 3 users by image score:**\n";
+            highestAvg.forEach((user, index) => {
+                output += index + ": " + user.username + " [" + user.total + "]\n"
+            });
+            output += "\n";
+        }
+        if(lowestAvg && lowestAvg.length > 0){
+            output += "**Bottom 3 users by image score:**\n";
+            lowestAvg.forEach((user, index) => {
+                output += index + ": " + user.username + " [" + user.total + "]\n"
+            });
+            output += "\n";
+        }
+        if(highestTotal && highestTotal.length > 0){
+            output += "**Top 3 users by average image score:**\n";
+            highestTotal.forEach((user, index) => {
+                output += index + ": " + user.username + " [" + (Math.round(user.total * 100) / 100) + "]\n"
+            });
+            output += "\n";
+        }
+        if(lowestTotal && lowestTotal.length > 0){
+            output += "**Bottom 3 users by average image score:**\n";
+            lowestTotal.forEach((user, index) => {
+                output += index + ": " + user.username + " [" + (Math.round(user.total * 100) / 100) + "]\n"
+            });
+            output += "\n";
+        }
+
+        return message.channel.sendMessage("```css\n" + output + "```");
+    })
 }
 
 module.exports = {
-    names: ["image-stats"],
+    names: ["imagestats"],
     description: "Some stats about the top saved images.",
     run
 };
