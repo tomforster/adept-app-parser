2a67dac3b20bb72a7df76a20183c3a65b0412c1f
Tom <tomforster@gmail.com>
2017-06-11 18:43:24 +0100

    update audit commands

--- discord/commands/audit.js
+++ discord/commands/audit.js
@@ -6,12 +6,12 @@ const config = require('../../config');
 
 function run(message, params) {
     if (config.auditUrls && config.auditUrls.length > 0){
-        let results = auditUrls.filter(auditUrl => (auditUrl.keyword.toLowerCase() === params[0]) ||
+        let results = config.auditUrls.filter(auditUrl => (auditUrl.keyword.toLowerCase() === params[0]) ||
         ((auditUrl.keyword.toLowerCase()+'s') === params[0]));
         if(results.length > 0){
             return message.reply("<" + results[0].url + ">");
         }else{
-            return config.auditUrls[0];
+            return  message.reply("<" + config.auditUrls[0].url + ">");
         }
     }
     return Promise.resolve();
