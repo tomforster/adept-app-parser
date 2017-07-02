79e911f42cd836ae2375625a7c7271bb95f1138b
Tom <tomforster@gmail.com>
2017-07-02 13:54:41 +0100

    Set api updates back to 5 mins

--- warcraftApiService.js
+++ warcraftApiService.js
@@ -120,7 +120,7 @@ let createGuildUri = function(guild, realm){
 };
 
 module.exports = function(guild, realm, bot){
-    let cronString = '*/1 * * * *';
+    let cronString = '*/5 * * * *';
     log.info("Adding scheduled task to retrieve guild stat info at", cronString);
     cron.schedule(cronString, () => {
         //lots of horrible catches, todo refactor this
